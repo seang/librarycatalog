b73c911b6cee6da63784b245064956fa45c5edce
Sean Gillespie <info@spg.business>
2020-07-28 14:11:56 -0400

    Search and refresh book results

--- circulation_desk/library_portal/src/App.js
+++ circulation_desk/library_portal/src/App.js
@@ -8,14 +8,16 @@ const { useState, useEffect } = React;
 function App() {
   const [books, setBooks] = useState([]);
   const [update,triggerUpdate] = useState(null);
-
- 
+  const [search, setSearch] = useState(null)
+  const handleSubmit = async (evt) => {
+triggerUpdate()
+	};
   useEffect(() => {
     const catalogRecursion = async (cursor,compiledBooks=[])=>{
       let isMounted = true;
       await fetchGraphQL(`
            query {
-            books${cursor?'('+'after: "'+cursor+'")':''}{
+            books${cursor?'('+'after: "'+cursor+'")': search?'(search:'+'"'+search+'")':""}{
               pageInfo {
                 startCursor
                 endCursor
@@ -36,7 +38,6 @@ function App() {
            }
          `)
           .then((response) => {
-            console.log(response);
             if (!isMounted) {
               return;
             }
@@ -57,7 +58,13 @@ function App() {
   return (
     <div className="App">
       <h1>Library Catalogue</h1>
+      <input
+      value={search} onChange={(e)=>{setSearch(e.target.value)}}
+      ></input><button onClick={handleSubmit}>Search Catalogue</button>
+      <br></br>
+      <br></br>
       <button onClick={()=>triggerUpdate(Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5))}>Refresh Catalogue</button>
+      
       {books &&
         books.map((book) => <Book update={()=>triggerUpdate("updated")} book={book.node} key={book.node.id}></Book>)}
     </div>
